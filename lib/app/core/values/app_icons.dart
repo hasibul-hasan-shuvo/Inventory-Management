3548c08aa0403deda4fbca38f2f6ae100774f2bc
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-01-18 19:07:15 +0600

    Project-setup: Fix - Favorite icon spelling mistake.
    TASK-19: https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_sprints/taskboard/InventoryManagementGture%20Team/InventoryManagementGture/Sprint%201?workitem=19

--- lib/app/core/values/app_icons.dart
+++ lib/app/core/values/app_icons.dart
@@ -1,5 +1,5 @@
 abstract class AppIcons {
-  static const String favourite = "ic_favourite.svg";
+  static const String favourite = "ic_favorite.svg";
   static const String home = "ic_home.svg";
   static const String settings = "ic_settings.svg";
 }
