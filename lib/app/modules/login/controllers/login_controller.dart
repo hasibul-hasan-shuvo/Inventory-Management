9d8bb25cc2462f7eadecd615bb2277711f5d6894
Md. Shahin Bashar <sbiitju@gmail.com>
2024-01-25 20:03:14 +0600

    Login Page Design: Login Controller code refactoring
    Task-20: https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_workitems/edit/20

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -1,11 +1,5 @@
 import 'package:dental_inventory/app/core/base/base_controller.dart';
-import 'package:get/get.dart';
 
 class LoginController extends BaseController {
-  //TODO: Implement LoginController
 
-  final count = 0.obs;
-
-
-  void increment() => count.value++;
 }
