a5831447cdc4c45fc3a9a71d3749533cc7b3c1ef
Md. Shahin Bashar <sbiitju@gmail.com>
2024-02-27 17:21:28 +0600

    Inventory API Integration: remove github implementation
    Task-65: https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_workitems/edit/65/

--- lib/app/bindings/remote_source_bindings.dart
+++ lib/app/bindings/remote_source_bindings.dart
@@ -4,22 +4,15 @@ import 'package:dental_inventory/app/data/remote/inventory_remote_datasource.dar
 import 'package:dental_inventory/app/data/remote/inventroy_remote_datasource_imp.dart';
 import 'package:get/get.dart';
 
-import '/app/data/remote/github_remote_data_source.dart';
-import '/app/data/remote/github_remote_data_source_impl.dart';
-
 class RemoteSourceBindings implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<GithubRemoteDataSource>(
-      () => GithubRemoteDataSourceImpl(),
-      tag: (GithubRemoteDataSource).toString(),
-    );
     Get.lazyPut<AuthRemoteDataSource>(
       () => AuthRemoteDataSourceImp(),
       fenix: true,
     );
     Get.lazyPut<InventoryRemoteDataSource>(
-          () => InventoryRemoteDataSourceImp(),
+      () => InventoryRemoteDataSourceImp(),
       fenix: true,
     );
   }
--- lib/app/bindings/repository_bindings.dart
+++ lib/app/bindings/repository_bindings.dart
@@ -4,14 +4,9 @@ import 'package:dental_inventory/app/data/repository/login_repository.dart';
 import 'package:dental_inventory/app/data/repository/login_repository_imp.dart';
 import 'package:get/get.dart';
 
-import '/app/data/repository/github_repository.dart';
-import '/app/data/repository/github_repository_impl.dart';
-
 class RepositoryBindings implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<GithubRepository>(() => GithubRepositoryImpl(),
-        tag: (GithubRepository).toString(), fenix: true);
     Get.lazyPut<AuthRepository>(() => AuthRepositoryImp(), fenix: true);
 
     Get.lazyPut<InventoryRepository>(() => InventoryRepositoryImp(),
