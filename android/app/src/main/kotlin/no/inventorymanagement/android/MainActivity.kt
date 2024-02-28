a9e0e9139a67ffc67d57371507ac64cc4d4d7b3e
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-02-28 19:36:49 +0600

    Zebra-SDK: Android MethodChannel implemented.
    TASK-64: https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_sprints/taskboard/InventoryManagementGture%20Team/InventoryManagementGture/Sprint%204?workitem=64

--- android/app/src/main/kotlin/no/inventorymanagement/android/MainActivity.kt
+++ android/app/src/main/kotlin/no/inventorymanagement/android/MainActivity.kt
@@ -1,6 +1,38 @@
 package no.inventorymanagement.android
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "inventorymanagement.no/zebra-scanner"
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            call.method?.let { method ->
+//                val uuid: String? = call.argument("uuid")
+//                val apiPublicKey: String? = call.argument("apiPublicKey")
+//                val mKey: String? = call.argument("mKey")
+//
+//                if (!uuid.isNullOrEmpty() && !apiPublicKey.isNullOrEmpty()) {
+//                    try {
+//                        val claySDK: IClaySDK? = ClaySDK.init(this, apiPublicKey, uuid)
+//                        if (method == "get_public_key") {
+//                            result.success(claySDK?.publicKey)
+//                        } else if (method == "open_door") {
+//                            mKey?.let { key ->
+//                                openDoor(claySDK, key, result)
+//                            }
+//                        }
+//                    } catch (e: ClayException) {
+//                        result.error(e.errorCode.toString(), e.message, "Clay SDK init Exception")
+//                    }
+//
+//                }
+            }
+        }
+    }
 }
