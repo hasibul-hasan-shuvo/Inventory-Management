73b73b34ac7b81f37c7dc90704342c774cdfbffb
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-03-21 00:08:39 +0600

    Fix: Code refactored.
    TASK-58: https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_sprints/taskboard/InventoryManagementGture%20Team/InventoryManagementGture/Sprint%205?workitem=58

--- lib/app/data/model/response/inventory_response.dart
+++ lib/app/data/model/response/inventory_response.dart
@@ -1,4 +1,4 @@
-
+import 'package:dental_inventory/app/data/model/response/product_response.dart';
 
 class InventoryListResponse {
   InventoryListResponse.fromJson(dynamic json) {
@@ -21,7 +21,7 @@ class InventoryListResponse {
 
 class InventoryResponse {
   int? id;
-  Product? product;
+  ProductResponse? product;
   String? created;
   String? modified;
   int? maxCount;
@@ -30,21 +30,11 @@ class InventoryResponse {
   int? fixedSuggestion;
   int? inventory;
 
-  InventoryResponse(
-      {this.id,
-      this.product,
-      this.created,
-      this.modified,
-      this.maxCount,
-      this.minCount,
-      this.stockCount,
-      this.fixedSuggestion,
-      this.inventory});
-
   InventoryResponse.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    product =
-        json['product'] != null ? Product.fromJson(json['product']) : null;
+    product = json['product'] != null
+        ? ProductResponse.fromJson(json['product'])
+        : null;
     created = json['created'];
     modified = json['modified'];
     maxCount = json['max_count'];
@@ -52,48 +42,5 @@ class InventoryResponse {
     stockCount = json['stock_count'];
     fixedSuggestion = json['fixed_suggestion'];
     inventory = json['inventory'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    if (product != null) {
-      data['product'] = product!.toJson();
-    }
-    data['created'] = created;
-    data['modified'] = modified;
-    data['max_count'] = maxCount;
-    data['min_count'] = minCount;
-    data['stock_count'] = stockCount;
-    data['fixed_suggestion'] = fixedSuggestion;
-    data['inventory'] = inventory;
-
-    return data;
-  }
-}
-
-class Product {
-  int? id;
-  String? name;
-  String? itemId;
-  String? imageUrl;
-
-  Product({this.id, this.name, this.itemId, this.imageUrl});
-
-  Product.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    itemId = json['item_id'];
-    imageUrl = json['image_url'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['name'] = name;
-    data['item_id'] = itemId;
-    data['image_url'] = imageUrl;
-
-    return data;
   }
 }
