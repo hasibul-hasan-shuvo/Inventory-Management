3bea583c88acd3a4308e630f2cf8a5d3562aa29c
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-03-25 17:25:51 +0600

    Item-count- get product API integrated and onScanned function implemented.
    TASK-114: https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_sprints/taskboard/InventoryManagementGture%20Team/InventoryManagementGture/Sprint%206?workitem=114

--- lib/app/modules/item_count/controllers/item_count_controller.dart
+++ lib/app/modules/item_count/controllers/item_count_controller.dart
@@ -1,3 +1,45 @@
 import 'package:dental_inventory/app/core/base/base_controller.dart';
+import 'package:dental_inventory/app/core/values/string_extensions.dart';
+import 'package:dental_inventory/app/data/model/response/inventory_response.dart';
+import 'package:dental_inventory/app/data/repository/inventory_repository.dart';
+import 'package:dental_inventory/app/modules/inventory/model/inventory_card_model.dart';
+import 'package:get/get.dart';
 
-class ItemCountController extends BaseController {}
+class ItemCountController extends BaseController {
+  final InventoryRepository _repository = Get.find();
+
+  final RxList<InventoryCardUIModel> _inventoriesController =
+      RxList.empty(growable: true);
+
+  List<InventoryCardUIModel> get inventories => _inventoriesController;
+
+  void onScanned(String? code) {
+    if (code.isNotNullOrEmpty) {
+      bool isListItem = false;
+      for (InventoryCardUIModel product in inventories) {
+        if (product.productCode == code) {
+          isListItem = true;
+          // product.updateNumber(product.number + 1);
+          break;
+        }
+      }
+      if (!isListItem) {
+        _getProduct(code!);
+      }
+
+      _inventoriesController.refresh();
+    }
+  }
+
+  void _getProduct(String itemId) {
+    callDataService(
+      _repository.getProduct(itemId),
+      onSuccess: _handleGetProductSuccessResponse,
+    );
+  }
+
+  void _handleGetProductSuccessResponse(InventoryResponse response) {
+    _inventoriesController
+        .add(InventoryCardUIModel.fromInventoryResponse(response));
+  }
+}
