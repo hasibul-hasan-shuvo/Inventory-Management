299844c7c7a8fdfbe7bfb729f50d77bc6ad2ef9d
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-04-10 05:53:52 +0600

    Version - App version showing in AboutApp page.

--- lib/app/modules/about_us/controllers/about_app_controller.dart
+++ lib/app/modules/about_us/controllers/about_app_controller.dart
@@ -1,3 +1,22 @@
+import 'package:get/get.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
 import '/app/core/base/base_controller.dart';
 
-class AboutAppController extends BaseController {}
+class AboutAppController extends BaseController {
+  final RxString _versionController = RxString('');
+
+  String get version => _versionController.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _getAppVersion();
+  }
+
+  void _getAppVersion() {
+    PackageInfo.fromPlatform().then((info) {
+      _versionController("${appLocalization.version}: ${info.version}");
+    });
+  }
+}
