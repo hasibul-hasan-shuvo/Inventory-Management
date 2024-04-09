7d4fc12e4be3b610f979be1cee303e590dd00375
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-04-09 14:42:29 +0600

    Fix - TextFieldWithTitle widget title is optional.

--- lib/app/modules/inventory/widget/text_field_with_title.dart
+++ lib/app/modules/inventory/widget/text_field_with_title.dart
@@ -1,4 +1,5 @@
 import 'package:dental_inventory/app/core/base/base_widget_mixin.dart';
+import 'package:dental_inventory/app/core/values/string_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -6,15 +7,16 @@ import '../../../core/values/app_values.dart';
 
 // ignore: must_be_immutable
 class TextFieldWithTitle extends StatelessWidget with BaseWidgetMixin {
-  final String title;
+  final String? title;
   final String initialValue;
   final Function(String) onChanged;
 
-  TextFieldWithTitle(
-      {required this.title,
-      required this.initialValue,
-      required this.onChanged,
-      Key? key});
+  TextFieldWithTitle({
+    required this.title,
+    required this.initialValue,
+    required this.onChanged,
+    Key? key,
+  });
 
   @override
   Widget body(BuildContext context) {
@@ -23,10 +25,12 @@ class TextFieldWithTitle extends StatelessWidget with BaseWidgetMixin {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text(title),
-        const SizedBox(
-          width: AppValues.margin_10,
-        ),
+        if (title.isNotNullOrEmpty)
+          Text(
+            title ?? '',
+            style: textTheme.bodyMedium,
+          ),
+        if (title.isNotNullOrEmpty) const SizedBox(width: AppValues.margin_10),
         SizedBox(
             width: AppValues.margin_40.w,
             height: AppValues.margin_30.w,
