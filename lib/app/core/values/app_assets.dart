428bcbc9d4e72d3b9ae9839503053276faa3f59e
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-05-28 14:23:32 +0600

    Offline-support: Account setup animation implemented.
    TASK-250: https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_sprints/taskboard/InventoryManagementGture%20Team/InventoryManagementGture/Sprint%2010?workitem=250

---
+++ lib/app/core/values/app_assets.dart
@@ -0,0 +1,5 @@
+abstract class AppAssets {
+  static const String _root = "assets";
+  static const String accountSetupAnimation =
+      "$_root/account_setup_animation.json";
+}
--- lib/app/modules/account_setup/views/account_setup_view.dart
+++ lib/app/modules/account_setup/views/account_setup_view.dart
@@ -1,5 +1,10 @@
 import 'package:dental_inventory/app/core/base/base_view.dart';
+import 'package:dental_inventory/app/core/values/app_assets.dart';
+import 'package:dental_inventory/app/core/values/app_colors.dart';
+import 'package:dental_inventory/app/core/values/app_values.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
 
 import '../controllers/account_setup_controller.dart';
 
@@ -9,7 +14,37 @@ class AccountSetupView extends BaseView<AccountSetupController> {
   PreferredSizeWidget? appBar(BuildContext context) => null;
 
   @override
+  Color pageBackgroundColor() {
+    return AppColors.primary;
+  }
+
+  @override
   Widget body(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(height: AppValues.margin.h),
+        _getLoaderView(),
+        SizedBox(height: AppValues.margin.h),
+        _getMessageView(),
+        SizedBox(height: AppValues.margin.h),
+      ],
+    );
+  }
+
+  Widget _getLoaderView() {
+    return Lottie.asset(
+      AppAssets.accountSetupAnimation,
+      repeat: true,
+    );
+  }
+
+  Widget _getMessageView() {
+    return Text(
+      appLocalization.messageAccountSetup,
+      style: textTheme.headlineSmall?.copyWith(color: AppColors.colorWhite),
+      textAlign: TextAlign.center,
+    );
   }
 }
