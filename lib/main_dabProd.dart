8d7b511b852e1db147156e61ecad42c8bca7220d
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-07-08 18:39:22 +0600

    DAB-Build-flavor: dab main function created.
    TASK 361: https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_sprints/taskboard/DABdental%20Team/InventoryManagementGture/DAB%20sprint%201?workitem=361

---
+++ lib/main_dabProd.dart
@@ -0,0 +1,37 @@
+import 'package:dental_inventory/app/core/services/firebase_service.dart';
+import 'package:dental_inventory/app/core/values/environment_values.dart';
+import 'package:dental_inventory/app/data/local/preference/preference_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '/app/my_app.dart';
+import '/flavors/build_config.dart';
+import '/flavors/env_config.dart';
+import '/flavors/environment.dart';
+
+void main() async {
+  EnvConfig devConfig = EnvConfig(
+    appName: "DAB Dev",
+    baseUrl: "https://devinventorymanagement.azurewebsites.net",
+    shouldCollectCrashLog: true,
+  );
+
+  BuildConfig.instantiate(
+    envType: Environment.DAB_PRODUCTION,
+    envConfig: devConfig,
+  );
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await ScreenUtil.ensureScreenSize();
+
+  await dotenv.load(fileName: "conf/.env", mergeWith: envValues);
+
+  await GetStorage.init(PreferenceManager.DATABASE_NAME);
+
+  FirebaseService.enableFirebase(Environment.DAB_PRODUCTION);
+
+  runApp(MyApp());
+}
