a14dcdedd43d67dcf043388f2f361c4ab3af5ff8
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-07-16 14:52:00 +0600

    Build-flavor: Dab apps created in firebase.
    TASK 361: https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_sprints/taskboard/DABdental%20Team/InventoryManagementGture/DAB%20sprint%201?workitem=361

--- lib/app/core/values/environment_values.dart
+++ lib/app/core/values/environment_values.dart
@@ -1,48 +1,46 @@
 Map<String, String> envValues = {
   // DAB Android prod environment variables
   "DAB_PROD_FIREBASE_ANDROID_API_KEY":
-      "AIzaSyCHeqRBGGtGn7SAXmXSzzJFXCey3c0Xq4s",
+      "AIzaSyDlv_OBFG8lg7kHTf_GliYWF3riX6_gApk",
   "DAB_PROD_FIREBASE_ANDROID_APP_ID":
-      "1:762030093709:android:11a4b647de9b4038e2b9c4",
-  "DAB_PROD_FIREBASE_ANDROID_MESSAGING_SENDER_ID": "762030093709",
-  "DAB_PROD_FIREBASE_ANDROID_PROJECT_ID": "inventorymanagement-54c7e",
+      "1:816265320071:android:03e66983707bf7281e8a22",
+  "DAB_PROD_FIREBASE_ANDROID_MESSAGING_SENDER_ID": "816265320071",
+  "DAB_PROD_FIREBASE_ANDROID_PROJECT_ID": "inventorymanagement-dab",
   "DAB_PROD_FIREBASE_ANDROID_STORAGE_BUCKET":
-      "inventorymanagement-54c7e.appspot.com",
+      "inventorymanagement-dab.appspot.com",
   "DAB_PROD_FIREBASE_ANDROID_DATABASE_URL":
-      "https://inventorymanagement-54c7e-default-rtdb.europe-west1.firebasedatabase.app",
+      "https://inventorymanagement-dab-default-rtdb.europe-west1.firebasedatabase.app",
 
   // DAB Android dev environment variables
-  "DAB_DEV_FIREBASE_ANDROID_API_KEY": "AIzaSyCHeqRBGGtGn7SAXmXSzzJFXCey3c0Xq4s",
+  "DAB_DEV_FIREBASE_ANDROID_API_KEY": "AIzaSyDlv_OBFG8lg7kHTf_GliYWF3riX6_gApk",
   "DAB_DEV_FIREBASE_ANDROID_APP_ID":
-      "1:762030093709:android:e952d4da18b15636e2b9c4",
-  "DAB_DEV_FIREBASE_ANDROID_MESSAGING_SENDER_ID": "762030093709",
-  "DAB_DEV_FIREBASE_ANDROID_PROJECT_ID": "inventorymanagement-54c7e",
+      "1:816265320071:android:50e2d7f644accc6b1e8a22",
+  "DAB_DEV_FIREBASE_ANDROID_MESSAGING_SENDER_ID": "816265320071",
+  "DAB_DEV_FIREBASE_ANDROID_PROJECT_ID": "inventorymanagement-dab",
   "DAB_DEV_FIREBASE_ANDROID_STORAGE_BUCKET":
-      "inventorymanagement-54c7e.appspot.com",
+      "inventorymanagement-dab.appspot.com",
   "DAB_DEV_FIREBASE_ANDROID_DATABASE_URL":
-      "https://inventorymanagement-54c7e-default-rtdb.europe-west1.firebasedatabase.app",
+      "https://inventorymanagement-dab-default-rtdb.europe-west1.firebasedatabase.app",
 
   // DAB IOS prod environment variables
-  "DAB_PROD_FIREBASE_IOS_API_KEY": "AIzaSyCHeqRBGGtGn7SAXmXSzzJFXCey3c0Xq4s",
-  "DAB_PROD_FIREBASE_IOS_APP_ID": "1:762030093709:ios:258ae35e6b663162e2b9c4",
-  "DAB_PROD_FIREBASE_IOS_MESSAGING_SENDER_ID": "762030093709",
-  "DAB_PROD_FIREBASE_IOS_PROJECT_ID": "inventorymanagement-54c7e",
-  "DAB_PROD_FIREBASE_IOS_STORAGE_BUCKET":
-      "inventorymanagement-54c7e.appspot.com",
-  "DAB_PROD_FIREBASE_IOS_BUNDLE_ID": "no.inventorymanagement.jacobsen",
+  "DAB_PROD_FIREBASE_IOS_API_KEY": "AIzaSyCa0a79BrnKtBtKehnl5mIrnfRY6B1LWbI",
+  "DAB_PROD_FIREBASE_IOS_APP_ID": "1:816265320071:ios:1f7cde3f53bd5b2d1e8a22",
+  "DAB_PROD_FIREBASE_IOS_MESSAGING_SENDER_ID": "816265320071",
+  "DAB_PROD_FIREBASE_IOS_PROJECT_ID": "inventorymanagement-dab",
+  "DAB_PROD_FIREBASE_IOS_STORAGE_BUCKET": "inventorymanagement-dab.appspot.com",
+  "DAB_PROD_FIREBASE_IOS_BUNDLE_ID": "no.inventorymanagement.dab",
   "DAB_PROD_FIREBASE_IOS_DATABASE_URL":
-      "https://inventorymanagement-54c7e-default-rtdb.europe-west1.firebasedatabase.app",
+      "https://inventorymanagement-dab-default-rtdb.europe-west1.firebasedatabase.app",
 
   // DAB IOS dev environment variables
-  "DAB_DEV_FIREBASE_IOS_API_KEY": "AIzaSyCHeqRBGGtGn7SAXmXSzzJFXCey3c0Xq4s",
-  "DAB_DEV_FIREBASE_IOS_APP_ID": "1:405784364085:ios:00397143778e3aca045d7c",
-  "DAB_DEV_FIREBASE_IOS_MESSAGING_SENDER_ID": "762030093709",
-  "DAB_DEV_FIREBASE_IOS_PROJECT_ID": "inventorymanagement-54c7e",
-  "DAB_DEV_FIREBASE_IOS_STORAGE_BUCKET":
-      "inventorymanagement-54c7e.appspot.com",
-  "DAB_DEV_FIREBASE_IOS_BUNDLE_ID": "no.inventorymanagement.app.dev",
+  "DAB_DEV_FIREBASE_IOS_API_KEY": "AIzaSyCa0a79BrnKtBtKehnl5mIrnfRY6B1LWbI",
+  "DAB_DEV_FIREBASE_IOS_APP_ID": "1:816265320071:ios:f38789318bfbb1cc1e8a22",
+  "DAB_DEV_FIREBASE_IOS_MESSAGING_SENDER_ID": "816265320071",
+  "DAB_DEV_FIREBASE_IOS_PROJECT_ID": "inventorymanagement-dab",
+  "DAB_DEV_FIREBASE_IOS_STORAGE_BUCKET": "inventorymanagement-dab.appspot.com",
+  "DAB_DEV_FIREBASE_IOS_BUNDLE_ID": "no.inventorymanagement.dab.dev",
   "DAB_DEV_FIREBASE_IOS_DATABASE_URL":
-      "https://inventorymanagement-54c7e-default-rtdb.europe-west1.firebasedatabase.app",
+      "https://inventorymanagement-dab-default-rtdb.europe-west1.firebasedatabase.app",
 
   // Jacobsen Android prod environment variables
   "JACOBSEN_PROD_FIREBASE_ANDROID_API_KEY":
@@ -90,7 +88,7 @@ Map<String, String> envValues = {
   "JACOBSEN_DEV_FIREBASE_IOS_PROJECT_ID": "inventorymanagement-54c7e",
   "JACOBSEN_DEV_FIREBASE_IOS_STORAGE_BUCKET":
       "inventorymanagement-54c7e.appspot.com",
-  "JACOBSEN_DEV_FIREBASE_IOS_BUNDLE_ID": "no.inventorymanagement.app.dev",
+  "JACOBSEN_DEV_FIREBASE_IOS_BUNDLE_ID": "no.inventorymanagement.jacobsen.dev",
   "JACOBSEN_DEV_FIREBASE_IOS_DATABASE_URL":
       "https://inventorymanagement-54c7e-default-rtdb.europe-west1.firebasedatabase.app",
 };
