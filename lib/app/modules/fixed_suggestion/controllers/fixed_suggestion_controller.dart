183b40074cd5edafca15cc0b9c685371ff1af3ab
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-09-18 18:49:53 +0600

    Fixed-suggestion: Fixed suggestion view and controller implemented.
    TASK 425: https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_sprints/taskboard/DABdental%20Team/InventoryManagementGture/DAB%20sprint%206?workitem=425

--- lib/app/modules/fixed_suggestion/controllers/fixed_suggestion_controller.dart
+++ lib/app/modules/fixed_suggestion/controllers/fixed_suggestion_controller.dart
@@ -1,3 +1,145 @@
 import 'package:dental_inventory/app/core/base/base_controller.dart';
+import 'package:dental_inventory/app/core/values/string_extensions.dart';
+import 'package:dental_inventory/app/data/model/request/add_shopping_cart_item_request_body.dart';
+import 'package:dental_inventory/app/data/model/response/inventory_response.dart';
+import 'package:dental_inventory/app/data/model/response/suggested_orders_response.dart';
+import 'package:dental_inventory/app/data/repository/suggested_orders_repository.dart';
+import 'package:dental_inventory/app/modules/suggested_orders/models/suggested_order_ui_model.dart';
+import 'package:get/get.dart';
 
-class FixedSuggestionController extends BaseController {}
+class FixedSuggestionController extends BaseController {
+  final SuggestedOrdersRepository _repository = Get.find();
+  final RxList<SuggestedOrderUiModel> _suggestedOrdersController =
+      RxList.empty(growable: true);
+
+  List<SuggestedOrderUiModel> get suggestedOrders => _suggestedOrdersController;
+
+  @override
+  void onInit() {
+    super.onInit();
+    pagingController.initRefresh();
+    _getSuggestedOrders();
+  }
+
+  @override
+  void onClose() {
+    for (var element in _suggestedOrdersController) {
+      element.close();
+    }
+    _suggestedOrdersController.close();
+    super.onClose();
+  }
+
+  void onLoading() {
+    if (pagingController.canLoadNextPage()) {
+      _getNextSuggestedOrders();
+    }
+  }
+
+  void _getSuggestedOrders() {
+    callDataService(
+      _repository.getSuggestedOrders(
+        pagingController.pageNumber,
+      ),
+      onSuccess: _handleSuggestedOrdersSuccessResponse,
+    );
+  }
+
+  void _handleSuggestedOrdersSuccessResponse(SuggestedOrdersResponse response) {
+    pagingController.nextPage();
+    pagingController.isLastPage = response.next == null;
+    List<SuggestedOrderUiModel> list = response.results
+            ?.map((e) => SuggestedOrderUiModel.fromSuggestedOrderResponse(e))
+            .toList() ??
+        [];
+
+    _suggestedOrdersController(list);
+  }
+
+  void _getNextSuggestedOrders() {
+    callDataService(
+      _repository.getSuggestedOrders(
+        pagingController.pageNumber,
+      ),
+      onSuccess: _handleNextSuggestedOrdersSuccessResponse,
+      onStart: () => logger.d("Fetching more suggested orders"),
+      onComplete: () => refreshController.loadComplete(),
+    );
+  }
+
+  void _handleNextSuggestedOrdersSuccessResponse(
+      SuggestedOrdersResponse response) {
+    pagingController.nextPage();
+    pagingController.isLastPage = response.next == null;
+    List<SuggestedOrderUiModel> list = suggestedOrders;
+    list.addAll(response.results
+            ?.map((e) => SuggestedOrderUiModel.fromSuggestedOrderResponse(e))
+            .toList() ??
+        []);
+  }
+
+  Future addToCart(String itemId, String quantityString) {
+    if (!quantityString.isPositiveIntegerNumber) {
+      showErrorMessage(appLocalization
+          .messageInvalidItemNumber(appLocalization.homeMenuShoppingCart));
+
+      return Future.value();
+    }
+
+    int quantity = quantityString.toInt;
+
+    AddShoppingCartItemRequestBody requestBody = AddShoppingCartItemRequestBody(
+      itemId: itemId,
+      quantity: quantity,
+    );
+
+    return callDataService(
+      _repository.addItemToShoppingCart(
+        requestBody,
+      ),
+      onSuccess: (_) => _handleAddToCartSuccessResponse(requestBody),
+    );
+  }
+
+  void _handleAddToCartSuccessResponse(AddShoppingCartItemRequestBody data) {
+    _suggestedOrdersController.removeWhere(
+      (element) => element.itemId == data.itemId,
+    );
+    showSuccessMessage(appLocalization.messageAddedToShoppingCart);
+  }
+
+  void addToCartAll() {
+    callDataService(
+      _repository.addAllItemsInShoppingCart(),
+      onSuccess: _handleAddToCartAllSuccessResponse,
+    );
+  }
+
+  void _handleAddToCartAllSuccessResponse(bool isSuccess) {
+    if (isSuccess) {
+      _suggestedOrdersController.clear();
+    }
+    showSuccessMessage(appLocalization.messageAddedToShoppingCart);
+  }
+
+  void rebuildList() {
+    _suggestedOrdersController.refresh();
+  }
+
+  void getProductPrice(SuggestedOrderUiModel data) {
+    if (data.price == 0.0) {
+      callDataService(
+        _repository.getSuggestedOrderWithPrice(data.itemId),
+        onSuccess: (response) =>
+            _handleGetProductPriceSuccessResponse(data, response),
+        onStart: () => logger.d("Fetched product price"),
+        onComplete: () => logger.d("Product price fetched"),
+      );
+    }
+  }
+
+  void _handleGetProductPriceSuccessResponse(
+      SuggestedOrderUiModel data, InventoryResponse response) {
+    data.updatePrice(response.product?.price);
+  }
+}
--- lib/app/modules/fixed_suggestion/views/fixed_suggestion_view.dart
+++ lib/app/modules/fixed_suggestion/views/fixed_suggestion_view.dart
@@ -1,34 +1,67 @@
 import 'package:dental_inventory/app/core/base/base_view.dart';
+import 'package:dental_inventory/app/core/values/app_values.dart';
 import 'package:dental_inventory/app/core/widget/custom_app_bar.dart';
+import 'package:dental_inventory/app/core/widget/paging_view.dart';
+import 'package:dental_inventory/app/modules/fixed_suggestion/widgets/item_fixed_suggestion_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 import '../controllers/fixed_suggestion_controller.dart';
 
 class FixedSuggestionView extends BaseView<FixedSuggestionController> {
   @override
   PreferredSizeWidget? appBar(BuildContext context) => CustomAppBar(
-        appBarTitleText: appLocalization.homeMenuSuggestedOrder,
+        appBarTitleText: appLocalization.homeMenuFixedSuggestion,
         actions: _getActions,
       );
 
   @override
   Widget body(BuildContext context) {
-    return const Center(
-      child: Text(
-        'FixedSuggestionView is working',
-        style: TextStyle(fontSize: 20),
-      ),
+    return Obx(
+      () => controller.isPageLoading && controller.suggestedOrders.isEmpty
+          ? const SizedBox.shrink()
+          : controller.suggestedOrders.isEmpty
+              ? _getPlaceHolder()
+              : PagingView(
+                  controller: controller.refreshController,
+                  enablePullDown: false,
+                  enablePullUp: controller.pagingController.canLoadNextPage(),
+                  onLoading: controller.onLoading,
+                  child: _getSuggestedOrdersListView(),
+                ),
     );
+  }
+
+  Widget _getPlaceHolder() {
+    return Container();
+  }
+
+  Widget _getSuggestedOrdersListView() {
+    return ListView.builder(
+      physics: const ClampingScrollPhysics(),
+      shrinkWrap: true,
+      padding: EdgeInsets.symmetric(
+        vertical: AppValues.padding.h,
+        horizontal: AppValues.margin.w,
+      ),
+      itemCount: controller.suggestedOrders.length,
+      itemBuilder: _getItemBuilder,
+    );
+  }
+
+  Widget _getItemBuilder(BuildContext context, int index) {
+    return ItemFixedSuggestionView(data: controller.suggestedOrders[index]);
   }
 
   List<Widget> get _getActions {
     return [
-      // IconButton(
-      //   onPressed: controller.addToCartAll,
-      //   icon: const Icon(
-      //     Icons.done,
-      //   ),
-      // )
+      IconButton(
+        onPressed: controller.addToCartAll,
+        icon: const Icon(
+          Icons.done,
+        ),
+      )
     ];
   }
 }
