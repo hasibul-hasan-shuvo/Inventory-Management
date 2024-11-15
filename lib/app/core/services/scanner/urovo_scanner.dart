dbf7e30cae078d38c95cb92d17bfc83ad26ff924
MD Hasibul Hasan Shuvo <Hasibul.Hasan@brainstation-23.com>
2024-11-15 15:12:58 +0600

    Urovo: ScanOutputMode setup and enable symbology functionality added with some refactor. And android:enableOnBackInvokedCallback made true for the application.
    TASK 453:
    https://dev.azure.com/InventoryManagementGture/InventoryManagementGture/_sprints/taskboard/DABdental%20Team/InventoryManagementGture/DAB%20-%20Sprint%2010?workitem=453

--- lib/app/core/services/scanner/urovo_scanner.dart
+++ lib/app/core/services/scanner/urovo_scanner.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:dental_inventory/app/core/services/scanner/scanner.dart';
 import 'package:laser_scanner/laser_scanner.dart';
 import 'package:laser_scanner/model/scan_result_model.dart';
+import 'package:laser_scanner/model/symbology_model.dart';
 import 'package:laser_scanner/utils/enum_utils.dart';
 
 class UrovoScanner extends Scanner {
@@ -22,21 +23,40 @@ class UrovoScanner extends Scanner {
   void _initUrovoScanner() async {
     _scanner.isSupport().then((bool? isSupport) {
       if (isSupport == true) {
-        _scanner.openScanner(captureImageShow: true).then((_) {
-          _setTrigger();
-          _enableVibrate();
-          _subscribeListener();
-        });
+        _scanner.openScanner(captureImageShow: true).then(onOpenCapture);
       }
     });
   }
 
-  void _setTrigger() {
-    _scanner.setTrigger(triggering: Triggering.PULSE);
+  void onOpenCapture(void value) async {
+    await _setTrigger();
+    await _enableVibrate();
+    await _enableSymbology();
+    await _setScanOutputMode();
+    _subscribeListener();
   }
 
-  void _enableVibrate() {
-    _scanner.enableVibrate();
+  Future<void> _setTrigger() {
+    return _scanner.setTrigger(triggering: Triggering.PULSE);
+  }
+
+  Future<void> _enableVibrate() {
+    return _scanner.enableVibrate();
+  }
+
+  Future<void> _enableSymbology() {
+    return _scanner.enableSymbology(
+      symbology: SymbologyModel(
+        enable: true,
+        symbology: symbologyValues[Symbology.QRCODE] ?? 31,
+      ),
+    );
+  }
+
+  Future<void> _setScanOutputMode() {
+    return _scanner.setScanOutputMode(
+      scanOutputMode: ScanOutputMode.decodeOuputModeIntent,
+    );
   }
 
   void _subscribeListener() async {
