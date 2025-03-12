19b862604740382d773fe73115bd4aa719705bf7
Yousuf Raza <yousuf.raza@brainstation-23.com>
2025-03-12 19:11:14 +0600

    Flutter version update cherry pick conflict resolved.

--- lib/app/modules/scanner/controllers/scanner_controller.dart
+++ lib/app/modules/scanner/controllers/scanner_controller.dart
@@ -1,3 +1,33 @@
 import 'package:dental_inventory/app/core/base/base_controller.dart';
+import 'package:dental_inventory/app/data/repository/app_settings_repository.dart';
+import 'package:get/get.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
-class ScannerController extends BaseController {}
+class ScannerController extends BaseController {
+  final AppSettingsRepository _repository = Get.find();
+
+  MobileScannerController scannerController = MobileScannerController(
+    detectionSpeed: DetectionSpeed.noDuplicates,
+  );
+
+  @override
+  void onInit() {
+    super.onInit();
+    _getCameraFacingStateAndSetToController();
+  }
+
+  void _getCameraFacingStateAndSetToController() {
+    CameraFacing cameraFacing = _repository.getScannerCameraFacingState();
+    scannerController = MobileScannerController(
+      detectionSpeed: DetectionSpeed.noDuplicates,
+      facing: cameraFacing,
+    );
+  }
+
+  void onPressedCameraSwitch() {
+    scannerController.switchCamera().then((_) {
+      _repository.saveScannerCameraFacingState(
+          scannerController.value.cameraDirection);
+    });
+  }
+}
